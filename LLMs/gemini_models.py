8c6378898db8d33e2c944528f2f61844ff84261c
karim <karimraoufm@gmail.com>
2025-12-01 20:49:28 +0200

    removed redundant

--- LLMs/gemini_models.py
+++ LLMs/gemini_models.py
@@ -6,20 +6,12 @@ gemini_llm = ChatGoogleGenerativeAI(
     api_key=os.getenv("GEMINI_API_KEY"),
     model="gemini-2.5-flash-lite",
     temperature=0.6,
-<<<<<<< HEAD
     timeout=300,
     max_retries=3,
-)
-=======
-    timeout=None,
-    max_retries=2,
 )
 
 gemini_embedding = GoogleGenerativeAIEmbeddings(
     model="models/gemini-embedding-001", 
     google_api_key=os.getenv("GEMINI_API_KEY") 
 )
-<<<<<<< HEAD
->>>>>>> c5cc8a00b674920893a03711ccfe2a7e80167f20
-=======
->>>>>>> c5cc8a00b674920893a03711ccfe2a7e80167f20
+
